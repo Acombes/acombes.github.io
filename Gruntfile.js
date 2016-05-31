f3ade10e94fc9a2be24edbd77e778a71fff8a3d3
ancom <antoine.combes.ac@gmail.com>
2016-05-31 14:34:03 +0200

    enabled livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,38 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    sass: {
-      options: {
-        sourcemap: "file"
-      },
-      dist: {
-        files: {
-          "stylesheets/styles.css": "stylesheets/styles.scss"
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourcemap: "file"
+            },
+            dist: {
+                files: {
+                    "stylesheets/styles.css": "stylesheets/styles.scss"
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: 35729
+            },
+            styles: {
+                files: ['stylesheets/**/*.scss', 'stylesheets/**/*.css', '!stylesheets/styles.css'],
+                tasks: ['styles']
+            }
+        },
+        serve: {
+            options: {
+                port: 8080
+            }
         }
-      }
-    },
-    watch: {
-      options: {
-        livereload: 35729
-      }
-      styles: {
-        files: ['stylesheets/*.scss', 'stylesheets/*.css', '!stylesheets/styles.css'],
-        tasks: ['styles']
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-serve');
 
 
-  grunt.registerTask('styles', ['sass']);
-  grunt.registerTask('default', ['styles']);
+    grunt.registerTask('styles', ['sass']);
+    grunt.registerTask('default', ['styles']);
 };
